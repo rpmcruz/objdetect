c4ff720def2cb635953eac2433a5c3f1426100d4
Ricardo Cruz <ricardo.pdm.cruz@gmail.com>
2022-09-30 01:38:21 +0100

    Convert to tensor on collate_fn.

--- src/objdetect/utils.py
+++ src/objdetect/utils.py
@@ -21,6 +21,6 @@ def plot(image, bboxes, labels=None, color='blue', linestyle='-', grid=None):
 def collate_fn(batch):
     '''The number of bounding boxes varies for each image, therefore the default PyTorch `collate` function (which creates the batches) must be replaced so that only images are turned into tensors.'''
     import torch
-    imgs = torch.stack([d['image'] for d in batch])
-    targets = {key: [d[key] for d in batch] for key in batch[0].keys()}
+    imgs = torch.stack([torch.as_tensor(d['image']) for d in batch])
+    targets = {key: [torch.as_tensor(d[key]) for d in batch] for key in batch[0].keys()}
     return imgs, targets
