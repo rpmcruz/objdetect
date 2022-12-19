42248104586ea482779f40e7a043065f28a7af92
Ricardo Cruz <ricardo.pdm.cruz@gmail.com>
2022-12-19 19:50:10 +0000

    Added filter_grid() helper function.

--- src/objdetect/utils.py
+++ src/objdetect/utils.py
@@ -23,3 +23,9 @@ def collate_fn(batch):
     imgs = torch.stack([torch.as_tensor(d['image']) for d in batch])
     targets = {key: [torch.as_tensor(d[key]) for d in batch] for key in batch[0].keys()}
     return imgs, targets
+
+def filter_grid(data, grid_min, grid_max):
+    '''Useful to filter objects when working with grids. The filter selects only objects when grid_min <= max(width, height) < grid_max.'''
+    ix = [[grid_min <= max(bb[2]-bb[0], bb[3]-bb[1]) < grid_max
+        for bb in bbs] for bbs in data['bboxes']]
+    return {key: [[e for i, e in zip(x, l) if i] for x, l in zip(ix, ll)] for key, ll in data.items()}
